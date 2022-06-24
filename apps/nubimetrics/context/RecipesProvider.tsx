5567cfcf762cb8fe883dc7e86b07a0737fb9d04b
Alex Ballera <alexballera@gmail.com>
2022-06-24 13:42:57 -0300

    Add custom hook

--- apps/nubimetrics/context/RecipesProvider.tsx
+++ apps/nubimetrics/context/RecipesProvider.tsx
@@ -1,3 +1,4 @@
+import { useFetch } from 'hooks/useFetch';
 import { useEffect, useState } from 'react';
 import { getRecipes } from 'services/recipe.service';
 import { RecipesContext } from './RecipesContext';
@@ -13,11 +14,15 @@ export interface RecipesProps {
 export const RecipesProvider = ({ children }) => {
   const [recipes, setRecipes] = useState<RecipesProps[]>();
   const [loading, setLoading] = useState<boolean>();
-  useEffect(() => {
-    getAllRecipes();
-  }, []);
+
+  // Custom hook useFetch
+  const { data, isLoading, hasError } = useFetch(
+    `${process.env.NEXT_PUBLIC_HOST}mock/recipes.json`
+  );
 
   function getAllRecipes() {
+    // Custom hook useFetch
+    console.log(data, isLoading, hasError);
     setLoading(true);
     getRecipes()
       .then((res) => {
@@ -27,6 +32,11 @@ export const RecipesProvider = ({ children }) => {
       .catch((err) => console.error(err))
       .finally(() => setLoading(false));
   }
+
+  useEffect(() => {
+    getAllRecipes();
+  }, []);
+
   return (
     <RecipesContext.Provider value={{ loading, recipes, setRecipes }}>
       {children}
